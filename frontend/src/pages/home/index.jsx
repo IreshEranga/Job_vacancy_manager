c464e240cef5c5880d7bd33a25715197aee9c0c0
Iresh Eranga <iresheranga2020al@gmail.com>
2024-06-11 21:56:27 +0530

    Update index.jsx

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import NavBar from '../../components/Navbar'
+import timer from '../../assets/timer.mp4'
 
+
+import './Home.css';
 
 const Home = () => {
   return (
-    <div style={{backgroundColor:'black'}}>
+    <div>
         <NavBar />
+
+        {/* Video */}
+
+        <div className="homevideo">
+          <video width="60%" controls controlsList="nodownload" style={{marginLeft:'300px' }}>
+            
+            <source src={timer} type="video/mp4" />
+                
+          </video>
+        </div>
+        
     </div>
   )
 }
