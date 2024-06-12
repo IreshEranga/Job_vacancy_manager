8b4280448ae54c583a81822b8a0189271d76c841
Iresh Eranga <iresheranga2020al@gmail.com>
2024-06-12 16:43:29 +0530

    home body about complete

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavBar from '../../components/Navbar'
 import timer from '../../assets/timer.mp4'
-
+import japan from '../../assets/japan.jpg'
 
 import './Home.css';
 
@@ -18,6 +18,20 @@ const Home = () => {
             <source src={timer} type="video/mp4" />
                 
           </video>
+        </div>
+
+        <div className="bodycontainerhome">
+          <div className="homebodyabout">
+            <h1>Excited to Work in <b>Japan</b> ? Discover Your Dream Job!</h1><br /><br />
+            <div className="imgwithpara">
+              <p className='homebodypara'>Japan is a land of innovation, rich culture, and endless opportunities. Whether you’re drawn to the bustling cities, advanced technology, or serene landscapes, Japan offers a unique and fulfilling work environment. Embrace the chance to grow professionally while experiencing the distinctive blend of tradition and modernity that defines Japanese life. Explore diverse job openings across industries, and join a community of forward-thinking professionals. Your next adventure awaits in Japan—start your journey today!</p>
+              <div className="imgbg">
+              <img src={japan} alt="japan" style={{borderRadius:'15px'}}/>
+              </div>
+            </div>
+          </div>
+
+          <br /><br /><br /><br />
         </div>
         
     </div>
