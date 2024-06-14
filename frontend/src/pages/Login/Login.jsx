6b6d5fb783a403a638bcce8b13c93b7e1911c122
Iresh Eranga <iresheranga2020al@gmail.com>
2024-06-14 22:07:56 +0530

    Create Login.jsx

---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { MDBContainer, MDBInput, MDBCheckbox, MDBBtn } from 'mdb-react-ui-kit';
+import NavBar from "../../components/Navbar_customer.js";
+
+
+function LogIn() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({ username: '', password: '' });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleLogin = () => {
+    // Hardcoded credentials
+    const hardcodedUsername = 'shenal';
+    const hardcodedPassword = 'vertex';
+
+    if (formData.username === hardcodedUsername && formData.password === hardcodedPassword) {
+      // Redirect to the desired page upon successful login
+      navigate('/admin');
+    } else {
+      alert('Invalid credentials. Please try again.');
+    }
+  };
+
+  return (
+    <div>
+        <NavBar/>
+        <MDBContainer className="p-3 my-5 d-flex flex-column w-50" style={{backgroundColor:'rgba(4, 29, 113, 0.841)', color:'white'}}>
+      <MDBInput
+        wrapperClass='mb-4'
+        label='Email address'
+        id='form1'
+        type='email'
+        name='username'
+        value={formData.username}
+        onChange={handleInputChange}
+        style={{marginTop:'20px'}}
+      />
+      <MDBInput
+        wrapperClass='mb-4'
+        label='Password'
+        id='form2'
+        type='password'
+        name='password'
+        value={formData.password}
+        onChange={handleInputChange}
+      />
+
+      <div className="d-flex justify-content-between mx-3 mb-4">
+        <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+      </div>
+
+      <MDBBtn className="mb-4" onClick={handleLogin}>Sign in</MDBBtn>
+    </MDBContainer>
+    </div>
+  );
+}
+
+export default LogIn;
